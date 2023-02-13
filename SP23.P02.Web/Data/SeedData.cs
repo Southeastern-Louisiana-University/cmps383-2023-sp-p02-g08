04d2a8d380e8d7d45e44a788042e2b8196bf49cb
Dawso125 <dawson.davis@selu.edu>
2023-02-13 13:52:28 -0600

    role seeding, making new branch for it

---
+++ SP23.P02.Web/Data/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SP23.P02.Web.Features.TrainStations;
+
+namespace SP23.P02.Web.Data
+{
+    public static class SeedData
+    {
+        public static async Task MigrateAndSeed(DataContext dataContext)
+        {
+            await dataContext.Database.MigrateAsync();
+
+            var trainStations = dataContext.Set<TrainStation>();
+
+            if (!await trainStations.AnyAsync())
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    dataContext.Set<TrainStation>()
+                        .Add(new TrainStation
+                        {
+                            Name = "Hammond",
+                            Address = "1234 Place st"
+                        });
+                }
+
+                await dataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
+    
