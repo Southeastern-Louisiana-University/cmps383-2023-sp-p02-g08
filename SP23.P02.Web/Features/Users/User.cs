94017b1a8f2f673aac561f2212065b5dcd1745df
Dawso125 <dawson.davis@selu.edu>
2023-02-15 14:57:29 -0600

    passed datacontext HasUser TEST

--- SP23.P02.Web/Features/Users/User.cs
+++ SP23.P02.Web/Features/Users/User.cs
@@ -1,7 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 public class User : IdentityUser<int>
 {
-   
+    public ICollection<UserRole> Users { get; set; } = new List<UserRole>();
 
+}
+public class CreateUserDto
+{
+
+
+    [Required]
+    public string UserName { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+    [Required, MinLength(1)]
+    public string[] Roles { get; set; } = Array.Empty<string>();
 }
